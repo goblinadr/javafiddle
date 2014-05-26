fd54a4b46170336f814fc26d9b621ad0c240a5e8
Vadim <rosso-corsa@yandex.ru>
2014-05-26 23:59:00 +0400

    Added function AddElement in ejbBean

--- JavaFiddle-ejb/src/java/com/javafiddle/core/ejb/UserProjectsBean.java
+++ JavaFiddle-ejb/src/java/com/javafiddle/core/ejb/UserProjectsBean.java
@@ -5,6 +5,7 @@ import com.javafiddle.core.jpa.Files;
 import com.javafiddle.core.jpa.Permissions;
 import com.javafiddle.core.jpa.User;
 import java.io.Serializable;
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 import javax.ejb.Stateful;
@@ -20,33 +21,98 @@ public class UserProjectsBean implements UserProjectsBeanLocal, Serializable {
 
     @PersistenceContext
     private EntityManager em;
-    private LinkedList< Element> projects;
+    private LinkedList<Element> projects;
+
+    public void addElement(String name, String type, String hash, Long parentId, Long creatorId) {
+        Files fl = new Files();
+        fl.setDate(new Date());
+        fl.setFilePath(hash);
+        fl.setName(name);
+        fl.setType(type);
+        List<User> creator = em.createQuery("select u from User u where u.id =:UsId").setParameter("UsId", creatorId).getResultList();
+        if (creator.size() == 1) {
+            fl.setCreator(creator.get(0));
+        }
+        if (type.equals("Project") || type.equals("Maven Project")) {
+            fl.setParent(null);
+            em.persist(fl);
+            Element newEl = new Element(fl.getId(), name, type, true, hash);
+            projects.add(newEl);
+        }
+        else
+        {
+            List <Files> parentFile = em.createQuery("select u from Files u where u.id =:parId")
+                    .setParameter("parId", parentId).getResultList();
+            if (parentFile.size() == 1){
+                fl.setParent(parentFile.get(0));
+            }
+            em.persist(fl);
+            Element newEl = new Element(fl.getId(), name, type, true, hash);
+            for ( int i = 0; i < projects.size(); i++)
+            {
+                if ( projects.get(i).id == parentId )
+                {
+                    projects.get(i).child.add(newEl);
+                    break;
+                }
+                else
+                {
+                    Element el = findElemById(parentId, projects.get(i));
+                    if ( el != null)
+                    {
+                        el.child.add(newEl);
+                    }
+                }
+            }
+        }
+//        Element newEl = new Element()
+    }
+
+    public void deleteElement(Element el) {
+
+    }
+
+    private Element findElemById(Long idForSearch, Element curEl) {
+        for ( int i = 0; i < curEl.child.size(); i++)
+        {
+            if (curEl.child.get(i).id == idForSearch){
+                return curEl.child.get(i);
+            }
+            if (curEl.child.get(i).type.equals("Folder")){
+                Element el = findElemById(idForSearch, curEl.child.get(i));
+                if ( el != null) {
+                    return el;
+                }
+            }
+        }
+        return null;
+    }
 
     @Override
     public LinkedList< Element> getProjects(Long userId) {
         projects = new LinkedList<>();
         List<User> myUser = em.createQuery("select u from User u where u.id =:UsId").setParameter("UsId", userId).getResultList();
-        List<Files> projs = em.createQuery("select u from Files u where u.creator =:id AND u.type = 'Project'")
+        List<Files> projs = em.createQuery("select u from Files u where u.creator =:id AND (u.type = 'Project' OR u.type = 'Maven Project')")
                 .setParameter("id", myUser.get(0)).getResultList();
         if (projs != null) {
             for (int i = 0; i < projs.size(); i++) {
-                Element pr = new Element(projs.get(i).getId(), projs.get(i).getName(), "Project", true, projs.get(i).getFilePath());
+                Element pr = new Element(projs.get(i).getId(), projs.get(i).getName(), projs.get(i).getType(), true, projs.get(i).getFilePath());
                 projects.add(pr);
                 getChildren(pr, projs.get(i), i, -1L);
             }
         }
-        List <Permissions> pers = em.createQuery("select u from Permissions u where u.user =:id")
+        List<Permissions> pers = em.createQuery("select u from Permissions u where u.user =:id")
                 .setParameter("id", myUser.get(0)).getResultList();
         if (pers != null) {
             for (int i = 0; i < pers.size(); i++) {
                 Long fileId = pers.get(i).getId();
                 em.flush();
-                Files fls = em.find(Files.class, fileId);               
-                while (!fls.getType().equals("Project")) {
-                    List <Files> buf = em.createQuery("select u from Files u where u.id =: id ").setParameter("id", fls.getParent()).getResultList();
+                Files fls = em.find(Files.class, fileId);
+                while (!fls.getType().equals("Project") && !fls.getType().equals("Maven Project")) {
+                    List<Files> buf = em.createQuery("select u from Files u where u.id =: id ").setParameter("id", fls.getParent()).getResultList();
                     fls = buf.get(0);
                 }
-                Element pr = new Element(fls.getId(), fls.getName(), "Project", false, fls.getFilePath());
+                Element pr = new Element(fls.getId(), fls.getName(), fls.getType(), false, fls.getFilePath());
                 if (fileId == fls.getId()) {
                     pr.canEdit = true;
                 }
